c2e5e89ebe402dbe607c3b10608e12064f2bbb1c
1three <1_three@naver.com>
2023-11-15 20:48:36 +0900

    feat: 하단 네비바와 `Fragment` 연결

--- app/src/main/java/com/three/green_defense_force/MainActivity.kt
+++ app/src/main/java/com/three/green_defense_force/MainActivity.kt
@@ -2,10 +2,47 @@ package com.three.green_defense_force
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private val homeFragment = HomeFragment()
+    private val challengeFragment = ChallengeFragment()
+    private val ploggingFragment = PloggingFragment()
+    private val storeFragment = StoreFragment()
+    private val mypageFragment = MypageFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view)
+
+        // 초기 화면 : HomeFragment
+        replaceFragment(homeFragment)
+
+        // BottomNavigationView의 아이템 클릭 리스너 설정
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.nav_mypage -> mypageFragment
+                R.id.nav_plogging -> ploggingFragment
+                R.id.nav_home -> homeFragment
+                R.id.nav_store -> storeFragment
+                R.id.nav_challenge -> challengeFragment
+                else -> homeFragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+
+        // 홈 탭 : 기본 선택
+        bottomNavigationView.menu.findItem(R.id.nav_home)?.isChecked = true
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.bottom_nav_fragment, fragment)
+            .commit()
     }
 }
