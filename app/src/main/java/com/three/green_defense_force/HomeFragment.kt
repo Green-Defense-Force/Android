64a49187414750943595fd759d96dfa7172acc0a
1three <1_three@naver.com>
2023-11-16 12:56:20 +0900

    refactor: `게임 전환` 구성 변경에 따른 리팩토링

--- app/src/main/java/com/three/green_defense_force/HomeFragment.kt
+++ app/src/main/java/com/three/green_defense_force/HomeFragment.kt
@@ -1,12 +1,24 @@
 package com.three.green_defense_force
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class HomeFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val startBtn: Button = view.findViewById(R.id.startBtn)
+        startBtn.setOnClickListener {
+            val intent = Intent(activity, GameActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,7 +27,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
