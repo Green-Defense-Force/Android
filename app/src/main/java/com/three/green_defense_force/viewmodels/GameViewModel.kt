5fb5fd7fea2d5340419c5bc8256ec57fbd12f916
1three <1_three@naver.com>
2023-11-23 03:56:24 +0900

    refactor: GameViewModel 리팩토링

--- app/src/main/java/com/three/green_defense_force/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/three/green_defense_force/viewmodels/GameViewModel.kt
@@ -5,32 +5,39 @@ import com.three.green_defense_force.models.Game
 import com.three.green_defense_force.models.MonsterPreview
 
 class GameViewModel : ViewModel() {
-
     /** 서버에서 유저 정보 받아오는 함수 */
-    fun fetchUserData(userId: String): Game {
+    fun fetchGameData(userId: String): Game {
         val userLevel = 1
         val nickname = "three"
         val ticketAmount = 3
         val coinAmount = 100
         val bonusCoin = 3
         val monsterPreviews = listOf(
-            MonsterPreview("1", createImageUrl("game_skull.png")),
-            MonsterPreview("2", createImageUrl("game_garbage.png")),
+            MonsterPreview(
+                "1",
+                "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_skull.png?raw=true"
+            ),
+            MonsterPreview(
+                "2",
+                "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_garbage.png?raw=true"
+            ),
         )
         val characterImages = listOf(
-            createImageUrl("game_char_up.png"),
-            createImageUrl("game_char_up_walk1.png"),
-            createImageUrl("game_char_up_walk2.png"),
-            createImageUrl("game_char_down.png"),
-            createImageUrl("game_char_down_walk1.png"),
-            createImageUrl("game_char_down_walk2.png"),
-            createImageUrl("game_char_left.png"),
-            createImageUrl("game_char_left_walk1.png"),
-            createImageUrl("game_char_left_walk2.png"),
-            createImageUrl("game_char_right.png"),
-            createImageUrl("game_char_right_walk1.png"),
-            createImageUrl("game_char_right_walk2.png"),
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_up.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_up_walk1.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_up_walk2.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_down.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_down_walk1.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_down_walk2.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_left.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_left_walk1.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_left_walk2.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_right.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_right_walk1.png?raw=true",
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_char_right_walk2.png?raw=true",
         )
+        val field =
+            "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/game_bg_2.jpg?raw=true"
 
         return Game(
             userId,
@@ -40,11 +47,13 @@ class GameViewModel : ViewModel() {
             coinAmount,
             bonusCoin,
             monsterPreviews,
-            characterImages
+            characterImages,
+            field
         )
     }
 
-    private fun createImageUrl(imageName: String): String {
-        return "https://github.com/1three/1three-green-defense-force-image-api/blob/main/images/$imageName?raw=true"
+    /** 서버에 변경된 코인 정보를 전달하는 함수 */
+    fun handleCoinIntersect(game: Game) {
+        game.coinAmount += 1
     }
 }
