f4458c62c194175585c6d631f22e1928349d1b27
1three <1_three@naver.com>
2023-11-27 10:02:58 +0900

    feat: Custom 하단 네비바 구현

--- app/src/main/java/com/three/green_defense_force/activities/MainActivity.kt
+++ app/src/main/java/com/three/green_defense_force/activities/MainActivity.kt
@@ -2,50 +2,69 @@ package com.three.green_defense_force.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.three.green_defense_force.R
 import com.three.green_defense_force.fragments.ChallengeFragment
 import com.three.green_defense_force.fragments.HomeFragment
-import com.three.green_defense_force.fragments.MypageFragment
 import com.three.green_defense_force.fragments.PloggingFragment
-import com.three.green_defense_force.fragments.StoreFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var bottomNavigationView: LinearLayout
+    private lateinit var btnChallenge: Button
+    private lateinit var btnHome: Button
+    private lateinit var btnPlogging: Button
 
     private val homeFragment = HomeFragment()
     private val challengeFragment = ChallengeFragment()
     private val ploggingFragment = PloggingFragment()
-    private val storeFragment = StoreFragment()
-    private val mypageFragment = MypageFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bottomNavigationView = findViewById(R.id.bottom_nav_view)
+        btnChallenge = findViewById(R.id.nav_challenge)
+        btnHome = findViewById(R.id.nav_home)
+        btnPlogging = findViewById(R.id.nav_plogging)
 
         // 초기 화면 : HomeFragment
         replaceFragment(homeFragment)
 
-        // BottomNavigationView의 아이템 클릭 리스너 설정
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            val fragment = when (item.itemId) {
-                R.id.nav_mypage -> mypageFragment
-                R.id.nav_plogging -> ploggingFragment
-                R.id.nav_home -> homeFragment
-                R.id.nav_store -> storeFragment
-                R.id.nav_challenge -> challengeFragment
-                else -> homeFragment
-            }
-            replaceFragment(fragment)
-            true
+        // 버튼 클릭 리스너 설정
+        btnChallenge.setOnClickListener {
+            selectFragment(challengeFragment)
+        }
+        btnHome.setOnClickListener {
+            selectFragment(homeFragment)
+        }
+        btnPlogging.setOnClickListener {
+            selectFragment(ploggingFragment)
         }
 
         // 홈 탭 : 기본 선택
-        bottomNavigationView.menu.findItem(R.id.nav_home)?.isChecked = true
+        btnHome.isSelected = true
     }
+
+    private fun selectFragment(fragment: Fragment) {
+        replaceFragment(fragment)
+
+        // 선택한 버튼 스타일 변경
+        btnChallenge.setBackgroundResource(
+            if (fragment == challengeFragment) R.drawable.bottom_nav_item_selected
+            else R.drawable.bottom_nav_item_unselected
+        )
+        btnHome.setBackgroundResource(
+            if (fragment == homeFragment) R.drawable.bottom_nav_item_selected
+            else R.drawable.bottom_nav_item_unselected
+        )
+        btnPlogging.setBackgroundResource(
+            if (fragment == ploggingFragment) R.drawable.bottom_nav_item_selected
+            else R.drawable.bottom_nav_item_unselected
+        )
+    }
+
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.bottom_nav_fragment, fragment)
