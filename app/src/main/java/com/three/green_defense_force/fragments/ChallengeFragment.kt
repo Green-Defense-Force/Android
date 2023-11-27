870508038adbedc98aa78d7dbdf85655f317d893
1three <1_three@naver.com>
2023-11-27 10:16:16 +0900

    refactor: UI/UX 변경에 따라 Fragment 재구성

--- app/src/main/java/com/three/green_defense_force/fragments/ChallengeFragment.kt
+++ app/src/main/java/com/three/green_defense_force/fragments/ChallengeFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.three.green_defense_force.R
 
 class ChallengeFragment : Fragment() {
+    private val COLOR_CHALLENGE_TOP = R.color.challenge_top
+    private val COLOR_NAVI_BOTTOM = R.color.navi_bottom
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBarColor()
     }
 
     override fun onCreateView(
@@ -18,5 +23,13 @@ class ChallengeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_challenge, container, false)
+    }
+
+    /** 상태바 및 하단바 색상 지정하는 함수 */
+    private fun setBarColor() {
+        val window = requireActivity().window
+        val context = requireContext()
+        window.statusBarColor = ContextCompat.getColor(context, COLOR_CHALLENGE_TOP)
+        window.navigationBarColor = ContextCompat.getColor(context, COLOR_NAVI_BOTTOM)
     }
 }
--- app/src/main/java/com/three/green_defense_force/fragments/PloggingFragment.kt
+++ app/src/main/java/com/three/green_defense_force/fragments/PloggingFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.three.green_defense_force.R
 
 class PloggingFragment : Fragment() {
+    private val COLOR_PLOGGING_TOP = R.color.plogging_top
+    private val COLOR_NAVI_BOTTOM = R.color.navi_bottom
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBarColor()
     }
 
     override fun onCreateView(
@@ -19,5 +23,13 @@ class PloggingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plogging, container, false)
+    }
+
+    /** 상태바 및 하단바 색상 지정하는 함수 */
+    private fun setBarColor() {
+        val window = requireActivity().window
+        val context = requireContext()
+        window.statusBarColor = ContextCompat.getColor(context, COLOR_PLOGGING_TOP)
+        window.navigationBarColor = ContextCompat.getColor(context, COLOR_NAVI_BOTTOM)
     }
 }
